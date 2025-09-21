a48477cd0dedcc5b59d7791691f92d1213ba5509
Erick Nascimento <erickrochaluciano@gmail.com>
2025-09-21 20:36:57 -0300

    Criacao SegundoMaiorValor

---
+++ Loops e Array/SegundoMaiorValor.js
@@ -0,0 +1,17 @@
+let valores = [10, 5, 35, 20, 35, 15];
+let maior = valores[0];
+let segundoMaior = valores[0];   
+
+console.log(valores);
+
+for (let i = 0; i < valores.length; i++) {
+    if (valores[i] > maior) {
+        segundoMaior = maior;
+        maior = valores[i];
+    } else if (valores[i] > segundoMaior && valores[i] !== maior) {
+        segundoMaior = valores[i];
+    }
+}
+
+
+console.log(`O segundo maior valor é: ${segundoMaior}`);
